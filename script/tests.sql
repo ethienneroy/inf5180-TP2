7ea0e6cb3bdc8e35152c6f3e18156dc8b8842d08
Mathieu Mathurin <mathurin05@gmail.com>
2016-12-11 16:16:28 -0500

    ajout tests trigger nbConsultation

--- script/tests.sql
+++ script/tests.sql
@@ -182,13 +182,25 @@ ROLLBACK
 -- ****************************************************************************
 -- nbrMoyenMedicaments (nombre moyen de médicaments prescrits par un docteur),
 
+-- ****************************************************************************
 -- nbrConsultation (nombre total de consultations pour un patient),
-
+INSERT INTO Specialite VALUES(1, 'Elfe des bois', 'Vous ne passerez pas');
+INSERT INTO Docteur VALUES(1, 'Nom','prenom' , 1, 'Montreal', '103 A Rue Jarry H3K3F9', 'Docteur', 0, 0);
+INSERT INTO DossierPatient VALUES(5, 'nom', 'prenom', 'M', '123 456 789', TO_DATE('1999-12-31', 'yyyy-mm-dd'), TO_DATE('2016-12-11', 'yyyy-mm-dd'), 1, 0);
+INSERT INTO Consultation VALUES(1, 5, TO_DATE('2016-12-23', 'yyyy-mm-dd'), 'Mal de gorge', null);
+--Il devrait y avoir 1 consultation dans le dossier patient
+DECLARE nbConsultation INTEGER;
+Select NbrConsultation INTO nbConsultation FROM DossierPatient WHERE NumDos = 5;
+IF nbConsultation == 1 THEN
+    dbms_output.put_line('Success');
+ELSE
+    dbms_output.put_line('Fail');
+END IF;
 -- nbrMedicaments (nombre de medicaments differents – pas les boîtes- pour une unique ordonnance).
 
 -- ****************************************************************************
 -- La suppression d'un docteur doit entraîner la suppression de ses consultations.
-INSERT INTO Specialite VALUES(1, 'Elfe des bois', 'Vousne passerez pas');
+INSERT INTO Specialite VALUES(1, 'Elfe des bois', 'Vous ne passerez pas');
 INSERT INTO Docteur VALUES(1, 'Nom','prenom' , 1, 'Montreal', '103 A Rue Jarry H3K3F9', 'Docteur', 0, 0);
 INSERT INTO DossierPatient VALUES(5, 'nom', 'prenom', 'M', '123 456 789', TO_DATE('1999-12-31', 'yyyy-mm-dd'), TO_DATE('2016-12-11', 'yyyy-mm-dd'), 1, 0);
 INSERT INTO Consultation VALUES(1, 5, TO_DATE('2016-12-23', 'yyyy-mm-dd'), 'Mal de gorge', null);
