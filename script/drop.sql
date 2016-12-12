47e4f2d473d9a48c76430d4525c78eaeebf7264e
Mathieu Mathurin <mathurin05@gmail.com>
2016-12-11 23:32:51 -0500

    added drop for all triggers

--- script/drop.sql
+++ script/drop.sql
@@ -23,4 +23,21 @@ DROP TABLE Salle CASCADE CONSTRAINTS;
 DROP TABLE SpecialisationsSalle CASCADE CONSTRAINTS;
 DROP TABLE TypeChirurgie CASCADE CONSTRAINTS;
 
+DROP TRIGGER update_Docteur_dossierPatient;
+DROP TRIGGER unique_chirur_salle;
+DROP TRIGGER verif_sexe_patient;
+DROP TRIGGER verif_niveau_docteur;
+DROP TRIGGER verif_type_ordonnance;
+DROP TRIGGER verif_salle_operatoire;
+DROP TRIGGER verif_type_ordoChi;
+DROP TRIGGER verif_type_ordoMed;
+DROP TRIGGER nbrPatients_Docteur;
+DROP TRIGGER nbrMoyenMedicaments;
+DROP TRIGGER nbrConsultation_patient;
+DROP TRIGGER nbrMedicaments_ordonnance;
+DROP TRIGGER modif_Ord;
+DROP TRIGGER modif_Med;
+DROP TRIGGER cascade_modifie_docteur;
+DROP TRIGGER cascade_modifie_dossierPatient;
+
 SET ECHO OFF
